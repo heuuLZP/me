675994ffa0b97cc76a72398bc2f197ce0ea3e435
heuuLZP <heuuLZP@163.com>
2016-10-07 12:48:16 +0800

    添加导航

--- style/css/home.js
+++ style/css/home.js
@@ -6,56 +6,6 @@ const {width, height, scale} = Dimensions.get("window"),
     vmax = Math.max(vw, vh);
 
 export default StyleSheet.create({
-    "html": {
-        "paddingTop": 0,
-        "paddingRight": 0,
-        "paddingBottom": 0,
-        "paddingLeft": 0,
-        "marginTop": 0,
-        "marginRight": 0,
-        "marginBottom": 0,
-        "marginLeft": 0,
-        "height": "100%",
-        "font": "16px/1.5",
-        "color": "#333",
-        "background": "#f5f5d5"
-    },
-    "body": {
-        "paddingTop": 0,
-        "paddingRight": 0,
-        "paddingBottom": 0,
-        "paddingLeft": 0,
-        "marginTop": 0,
-        "marginRight": 0,
-        "marginBottom": 0,
-        "marginLeft": 0,
-        "height": "100%",
-        "font": "16px/1.5",
-        "color": "#333",
-        "background": "#f5f5d5"
-    },
-    "ul": {
-        "marginTop": 0,
-        "marginRight": 0,
-        "marginBottom": 0,
-        "marginLeft": 0,
-        "paddingTop": 0,
-        "paddingRight": 0,
-        "paddingBottom": 0,
-        "paddingLeft": 0
-    },
-    "a": {
-        "color": "#ff9999"
-    },
-    "main": {
-        "position": "relative",
-        "width": "80%",
-        "minHeight": "95%",
-        "marginTop": 0,
-        "marginRight": "auto",
-        "marginBottom": 0,
-        "marginLeft": "auto"
-    },
     "content": {
         "marginTop": 20,
         "paddingLeft": 100,
@@ -67,34 +17,5 @@ export default StyleSheet.create({
         "width": "100%",
         "height": 30,
         "textAlign": "center"
-    },
-    "h1": {
-        "marginTop": 0,
-        "marginRight": 0,
-        "marginBottom": 0,
-        "marginLeft": 0
-    },
-    "nav": {
-        "marginLeft": "50%",
-        "display": "inline-block"
-    },
-    "nav ul": {
-        "listStyle": "none"
-    },
-    "header": {
-        "display": "inline-block"
-    },
-    "nav ul li": {
-        "display": "inline-block",
-        "marginRight": 25
-    },
-    "nav ul li a": {
-        "fontSize": 18
-    },
-    "article-list": {
-        "listStyle": "none"
-    },
-    "article-list li a": {
-        "fontSize": 22
     }
 });
